7af5b3c3855e9ca06669beaf839487d87cc913f5
Chris Erwin <crerwin@gmail.com>
2015-12-12 12:52:24 -0500

    Moved input files into a folder

--- advent.py
+++ advent.py
@@ -1,17 +1,22 @@
+import os
 import day1
 import day2
 import day3
 
+def makefilepath(filename):
+    return os.path.join('inputs', filename)
+
+
 def execday1():
-    inputfilename = "day1input.txt"
+    inputfilename = makefilepath("day1input.txt")
     print(day1.textonly(inputfilename))
 
 def execday2():
-    inputfilename = "day2input.txt"
+    inputfilename = makefilepath("day2input.txt")
     print(day2.textonly(inputfilename))
 
 def execday3():
-    inputfilename = "day3input.txt"
+    inputfilename = makefilepath("day3input.txt")
     print(day3.textonly(inputfilename))
 
 execday3()
