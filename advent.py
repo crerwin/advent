db48820d78ea6d655994ad6416f6dd70ddba659e
Chris Erwin <crerwin@gmail.com>
2015-12-12 14:51:55 -0500

    day 3 part 2

--- advent.py
+++ advent.py
@@ -3,6 +3,7 @@ import day1
 import day2
 import day3
 
+
 def makefilepath(filename):
     return os.path.join('inputs', filename)
 
@@ -11,9 +12,11 @@ def execday1():
     inputfilename = makefilepath("day1input.txt")
     print(day1.textonly(inputfilename))
 
+
 def execday2():
     inputfilename = makefilepath("day2input.txt")
     print(day2.textonly(inputfilename))
+
 
 def execday3():
     inputfilename = makefilepath("day3input.txt")
--- day3.py
+++ day3.py
@@ -7,9 +7,8 @@ class Coordinates():
 
 
 class Person():
-    coords = Coordinates()
-
     def __init__(self):
+        self.coords = Coordinates()
         self.coords.x = 0
         self.coords.y = 0
 
@@ -26,20 +25,66 @@ class Person():
         self.coords.x -= 1
 
 
+class Dispatcher(Person):
+    # dispatcher is a class that 'impersonates' a person.  It keeps track of who is moving (Santa or RoboSanta)
+    # and coords is masked so that when the calling function asks, it gets the correct set of coords
+    # this assumes coords is asked for after moving.  Changing that would require refactoring.
+
+    def __init__(self):
+        self.santa = Person()
+        self.robo_santa = Person()
+        self.nexttomove = self.santa
+        self.lasttomove = self.robo_santa
+
+    def moveup(self):
+        self.nexttomove.moveup()
+        self.switchnexttomove()
+
+    def movedown(self):
+        self.nexttomove.movedown()
+        self.switchnexttomove()
+
+    def moveright(self):
+        self.nexttomove.moveright()
+        self.switchnexttomove()
+
+    def moveleft(self):
+        self.nexttomove.moveleft()
+        self.switchnexttomove()
+
+    def _get_coords(self):
+        return self.lasttomove.coords
+
+    def switchnexttomove(self):
+        # there's probably a better way to do this
+        if self.nexttomove == self.santa:
+            self.nexttomove = self.robo_santa
+            self.lasttomove = self.santa
+        elif self.nexttomove == self.robo_santa:
+            self.nexttomove = self.santa
+            self.lasttomove = self.robo_santa
+        else:
+            raise Exception('nexttomove is assigned to an impossible value?!')
+
+    coords = property(_get_coords)
+
+
+
 def textonly(inputfilename):
     file = open(inputfilename)
     content = file.read()
-    result = walk(content)
-    stringresult = "Number of houses visited: " + str(result)
+    santaresult = walk(content, "part1")
+    bothresult = walk(content, "part2")
+    stringresult = "Number of houses visited by Santa only: " + str(santaresult) + " number of houses visited by Both: " + str(bothresult)
     return stringresult
 
 
-def walk(content):
+def walk(content, part="part1"):
     if not content:
         # string is empty
         return 0
     else:
-        Santa = Person()
+        Santa = getpersontype(part)
         visitedhousesdict = {(Santa.coords.x, Santa.coords.y): 1}
         for char in content:
             try:
@@ -54,6 +99,15 @@ def walk(content):
             except ValueError:
                 raise
     return len(visitedhousesdict)
+
+
+def getpersontype(part):
+    if part == "part1":
+        return Person()
+    elif part == "part2":
+        return Dispatcher()
+    else:
+        raise ValueError('bad value for part')
 
 
 def moveperson(person, char):
