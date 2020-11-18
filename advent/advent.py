aaf7fcfa985df7825fa974ca1a08ce0b99283ac6
Chris Erwin <crerwin@gmail.com>
2020-11-18 17:04:16 -0500

    run black

--- advent/advent.py
+++ advent/advent.py
@@ -1,20 +1,27 @@
 import click
-from advent import advent2015, advent2016, advent2017, advent2018, advent2019, advent2020
+from advent import (
+    advent2015,
+    advent2016,
+    advent2017,
+    advent2018,
+    advent2019,
+    advent2020,
+)
 
 
 class AdventCalendar(object):
     # Each year is an Advent Calendar for organization purposes
     def __init__(self):
         self.days = {}
-        self.days['2015'] = advent2015.days
-        self.days['2016'] = advent2016.days
-        self.days['2017'] = advent2017.days
-        self.days['2018'] = advent2018.days
-        self.days['2019'] = advent2019.days
-        self.days['2020'] = advent2020.days
+        self.days["2015"] = advent2015.days
+        self.days["2016"] = advent2016.days
+        self.days["2017"] = advent2017.days
+        self.days["2018"] = advent2018.days
+        self.days["2019"] = advent2019.days
+        self.days["2020"] = advent2020.days
 
     def run_day(self, year, day, part):
-        return self.days[str(year)][day-1].part(part)
+        return self.days[str(year)][day - 1].part(part)
 
 
 def _run(year, day, part):
