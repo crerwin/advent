84a589450d3d8a8cdba68205f30778077dd2a299
Chris Erwin <crerwin@gmail.com>
2020-11-20 16:21:15 -0500

    add test for Day

---
+++ tests/test_day.py
@@ -0,0 +1,8 @@
+import unittest
+from advent import day
+
+
+class DayTest(unittest.TestCase):
+    def test_part_1(self):
+        test_day = day.Day()
+        self.assertEqual(test_day.part1(), "not yet implemented")
