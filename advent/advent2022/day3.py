c0e907aebd6d65556c6d04861735e7c58108b9b9
Chris Erwin <crerwin@gmail.com>
2022-12-20 09:23:13 -0500

    day3 testing and test coverage

--- advent/advent2022/day3.py
+++ advent/advent2022/day3.py
@@ -4,3 +4,32 @@ from advent.day import Day
 class Day3(Day):
     year = 2022
     day = 3
+
+
+def split_line(line: str) -> tuple[str, str]:
+    print(len(line))
+    if len(line) % 2 != 0:
+        raise ValueError(f"{line} does not have an even number of characters")
+    else:
+        return line[0:2], line[2:]
+
+
+def find_common_element(string1: str, string2: str) -> str:
+    if len(string1) != len(string2):
+        raise ValueError(f"Length of {string1} not equal to length of {string2}")
+    else:
+        common_set = set(string1) & set(string2)
+        if len(common_set) > 1:
+            raise ValueError(
+                f"Found more than one common character in {string1} & {string2}: {common_set}"
+            )
+        else:
+            common_str = ""
+            for s in common_set:
+                common_str = common_str + s
+            if len(common_str) > 1:
+                raise ValueError(
+                    f"Found more than one common character in {string1} & {string2}: {common_set}"
+                )
+            else:
+                return common_str
--- tests/test_2022day3.py
+++ tests/test_2022day3.py
@@ -9,3 +9,16 @@ class TestDay3(DayTest):
     test_day = day3.Day3()
     expected_part_1 = "Advent 2022 Day 3 part 1 not yet implemented."
     expected_part_2 = "Advent 2022 Day 3 part 2 not yet implemented."
+
+    def test_split_line(self):
+        with self.assertRaises(ValueError):
+            day3.split_line("abcde")
+
+        self.assertEqual(day3.split_line("abcd"), ("ab", "cd"))
+
+    def test_find_common_element(self):
+        with self.assertRaises(ValueError):
+            day3.find_common_element("abc", "de")
+
+        self.assertEqual("c", day3.find_common_element("abc", "dcf"))
+        self.assertEqual("c", day3.find_common_element("abc", "dfc"))
